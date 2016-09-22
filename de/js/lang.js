7c03dc885c7ade017e2389ad70194cd8c895e30e
Martin Schimak <martin.schimak@plexiti.com>
2016-09-22 19:17:27 +0200

    publish: fix(title): fix title of root index.html and individual pages
    generated from commit f836194283445ddb633fed532948da9598d6d672

--- de/js/lang.js
+++ de/js/lang.js
@@ -19,3 +19,28 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function redirect2language(contextPath) {
+    var contextHost = contextPath.substr(0, contextPath.length - 3);
+    if (document.referrer && !document.referrer.startsWith(contextHost)) {
+        var pathLanguage = contextPath.substr(contextPath.length - 3, 2);
+        var userLanguage = getUserLanguage();
+        if (pathLanguage !== userLanguage) {
+            var path = window.location.pathname.replace('/' + pathLanguage + '/', '/' + userLanguage + '/');
+            window.location.pathname = path;
+        }
+    }
+}
+
+function redirect2blog() {
+    var lang = getUserLanguage();
+    if (lang=="de") {
+        window.location.pathname = window.location.pathname + "de/blog/"
+    } else {
+        window.location.pathname = window.location.pathname + "en/blog/"
+    }
+}
+
+function getUserLanguage() {
+    return (getCookie('lang') || navigator.language || navigator.systemLanguage).toLowerCase().substr(0,2);
+}
